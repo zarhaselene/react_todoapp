5c8747f7536ddb4f3fdb0d55ab283e9314e7dd8f
Zarha <zarhaselene@gmail.com>
2025-01-20 13:27:03 +0100

    add, toggle, and remove functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // store the todos
+  const [todos, setTodos] = useState([]);
+  // manage text input
+  const [inputValue, setInputValue] = useState("");
+
+  // function to add todo
+  const addTodo = () => {
+    if (!inputValue.trim()) return;  // prevent adding empty string
+    const newTodo = {
+      id: Date.now(), // ID set by using the current time
+      title: inputValue,
+      done: false,
+    };
+    setTodos([...todos, newTodo]); // update array with new todo
+    setInputValue(""); // clear the input field
+  };
+
+  // function to change status of todo
+  const toggleDone = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  // function to remove a todo
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   return (
     <>
-      
+      <h1>hello</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
